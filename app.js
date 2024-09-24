f46da18ee96b5c2ae83f8867abb981205bb16fef
labib <labibkhotimil@gmail.com>
2024-09-24 14:35:40 +0700

    update appjs

--- app.js
+++ app.js
@@ -28,7 +28,9 @@ app.get('/labib', (req, res) => {
 
 app.get('/users', (req, res, next) => {
   const sql = "SELECT * FROM tb_data ORDER BY id desc"
-  connection.query(sql{})
+  connection.query(sql, (fields) => {
+      res.send('fields')
+  })
 });
 
 app.listen(process.env.APP_PORT, () => {
